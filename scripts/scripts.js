5fd1193a91708f7b965a3b772610dd303289a1a1
Eric Nguyen <tommybao77@gmail.com>
2023-11-21 19:16:53 -0500

    Update all starting file

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,3 @@
 /*
     JavaScript Basics tutorial
 */
-
-let textValue = "Hello World";
-const headingElement = document.querySelector('h1');
-headingElement.textContent = textValue;
-
-const imageElement = document.querySelector('img');
-//imageElement.style.display = "none";
-
-imageElement.classlist.add('gone');
